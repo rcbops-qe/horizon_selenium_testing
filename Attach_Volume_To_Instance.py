3c716bc691283117ae7e8b1bba63d093411132ba
weezer <weezer.su@gmail.com>
2015-10-19 14:35:10 -0500

    add attach_colume to instance and boot_instance_from_volume, update cinder_cackup and the config file.

--- Attach_Volume_To_Instance.py
+++ Attach_Volume_To_Instance.py
@@ -9,7 +9,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re, json
 
-class Cinder_Backup(unittest.TestCase):
+class Attach_Volume_To_Instance(unittest.TestCase):
     def setUp(self):
         with open('config.json', 'r') as f:
             self.config = json.load(f)
@@ -20,7 +20,7 @@ class Cinder_Backup(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
 
-    def test_Cinder_Backup(self):
+    def test_Attach_Volume_To_Instance(self):
         driver = self.driver
         driver.get(self.base_url + "/auth/login/")
         driver.find_element_by_id("id_username").clear()
@@ -30,8 +30,33 @@ class Cinder_Backup(unittest.TestCase):
         driver.find_element_by_id("loginBtn").click()
         driver.find_element_by_css_selector("dt").click()
         compute_status = driver.find_element_by_xpath("//h4[contains(. , 'Compute')]").get_attribute('class')
+        #creaet vm instance
         if compute_status != 'active':
             driver.find_element_by_xpath("//dd/div/h4[text()[contains(.,'Compute')]]").click()
+        driver.find_element_by_xpath("//dd/div//a[text()='Instances' and @href='/project/instances/']").click()
+        driver.find_element_by_xpath("//a[@href='/project/instances/launch']").click()
+        driver.find_element_by_id("id_name").clear()
+        driver.find_element_by_id("id_name").send_keys(self.config['test_vm_name'])
+        Select(driver.find_element_by_id("id_flavor")).select_by_index(1)
+        driver.find_element_by_css_selector("option[value=\"1\"]").click()
+        Select(driver.find_element_by_id("id_source_type")).select_by_visible_text("Boot from image")
+        driver.find_element_by_css_selector("option[value=\"image_id\"]").click()
+        Select(driver.find_element_by_id("id_image_id")).select_by_index(1)
+        driver.find_element_by_xpath("//ul[@role='tablist']//a[text()='Networking']").click()
+        select_network = driver.find_element_by_xpath("//ul[@id='selected_network']").text
+        if select_network == '':
+            driver.find_element_by_xpath("//ul[@id='available_network']/li[1]/a[1]").click()
+
+        driver.find_element_by_xpath("//input[@value='Launch']").click()
+
+        Success_alert = driver.find_element_by_xpath("//p[contains(text(), 'Launched instance')]")
+
+        if "Success" in Success_alert.text and self.config['test_vm_name'] in str(Success_alert.text):
+             print Success_alert.text
+        else:
+            raise
+        driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_vm_name'] + "')]/td[contains(text(), 'Running')]")
+        #Create Volume
         driver.find_element_by_xpath("//dd/div//a[text()='Volumes' and @href='/project/volumes/']").click()
         volumes_status = driver.find_element_by_xpath("//ul[@id='volumes_and_snapshots']//li[contains(., 'Volumes')]").get_attribute('class')
         if volumes_status != 'active':
@@ -49,35 +74,39 @@ class Cinder_Backup(unittest.TestCase):
         else:
             raise
         driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume'] + "')]/td[contains(text(), 'Available')]")
-
-        #Create cinder backup
+        #attach volume to vm instance
         driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume'] + "')]//a[@data-toggle='dropdown']").click()
-        driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume'] + "')]//a[contains(. , 'Create Backup')]").click()
-        driver.find_element_by_id("id_name").clear()
-        driver.find_element_by_id("id_name").send_keys(self.config['test_volume_backup'])
-        driver.find_element_by_xpath("//input[@type='submit' and @value='Create Volume Backup']").click()
-        Success_alert = driver.find_element_by_xpath("//p[contains(text(), 'Creating volume backup ')]")
-        print Success_alert.text
-        if "Success:" in Success_alert.text and self.config['test_volume_backup'] in str(Success_alert.text):
-             print Success_alert.text
+        driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume'] + "')]//a[contains(. , 'Manage Attachments')]").click()
+        driver.find_element_by_xpath("//select[@id='id_instance']/option[contains(., 'horizon')]").click()
+        driver.find_element_by_xpath("//input[@type='submit' and @value='Attach Volume']").click()
+        attach_message = driver.find_element_by_xpath("//p[contains(text(), 'Attaching volume')]")
+        if "Info:" in attach_message.text and self.config['test_volume'] in str(attach_message.text):
+             print attach_message.text
         else:
             raise
-        driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume_backup'] + "')]/td[contains(text(), 'Available')]")
-        #Delete Cinder backup
-        driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume_backup'] + "')]//input[@type='checkbox']").click()
-        driver.find_element_by_xpath("//button[contains(., 'Delete Volume Backups')]").click()
-        driver.find_element_by_xpath("//a[contains(text(), 'Delete Volume Backups')]").click()
-        terminate_status = driver.find_element_by_xpath("//p[contains(text(), 'Scheduled deletion of Volume Backup:')]")
-        print terminate_status.text
-        driver.find_element_by_xpath("//ul[@id='volumes_and_snapshots']//a[contains(., 'Volumes')]").click()
-        #delete Cinder Volume.
-        table_element = driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume'] + "')]")
+        cinder_element = driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume'] + "')]")
+        attached_info = "Attached to " + self.config['test_vm_name']
+        print cinder_element.get_attribute("textContent")
+        print "cao ni ma"
+        time.sleep(10)
+        #delete vm isntance, you cant delete the volme if the volume is been attached.
+        driver.find_element_by_xpath("//dd/div//a[text()='Instances' and @href='/project/instances/']").click()
+        table_element = driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_vm_name'] + "')]")
         print table_element.text
         table_element.find_element_by_xpath(".//input[@type='checkbox']").click()
+        driver.find_element_by_xpath("//button[contains(., 'Terminate Instances')]").click()
+        driver.find_element_by_xpath("//a[contains(text(), 'Terminate Instance')]").click()
+        terminate_status = driver.find_element_by_xpath("//p[contains(text(), 'Scheduled termination of Instance:')]")
+        print terminate_status.text
+        #delete cinder volume
+        driver.find_element_by_xpath("//dd/div//a[text()='Volumes' and @href='/project/volumes/']").click()
+        cinder_element = driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume'] + "')]")
+        cinder_element.find_element_by_xpath(".//input[@type='checkbox']").click()
         driver.find_element_by_xpath("//button[contains(., 'Delete Volumes')]").click()
         driver.find_element_by_xpath("//a[contains(text(), 'Delete Volumes')]").click()
         terminate_status = driver.find_element_by_xpath("//p[contains(text(), 'Scheduled deletion of Volume:')]")
         print terminate_status.text
+
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException, e: return False
--- Boot_Instance_From_Volume.py
+++ Boot_Instance_From_Volume.py
@@ -9,7 +9,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re, json
 
-class Cinder_Backup(unittest.TestCase):
+class Boot_Instance_From_Volume(unittest.TestCase):
     def setUp(self):
         with open('config.json', 'r') as f:
             self.config = json.load(f)
@@ -20,7 +20,7 @@ class Cinder_Backup(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
 
-    def test_Cinder_Backup(self):
+    def test_Boot_Instance_From_Volume(self):
         driver = self.driver
         driver.get(self.base_url + "/auth/login/")
         driver.find_element_by_id("id_username").clear()
@@ -38,40 +38,50 @@ class Cinder_Backup(unittest.TestCase):
             driver.find_element_by_xpath("//ul[@id='volumes_and_snapshots']//li[contains(., 'Volumes')]").click()
         driver.find_element_by_xpath("//a[@href='/project/volumes/create/']").click()
         driver.find_element_by_id("id_name").clear()
-        driver.find_element_by_id("id_name").send_keys(self.config['test_volume'])
-        Select(driver.find_element_by_id("id_volume_source_type")).select_by_visible_text("No source, empty volume")
+        driver.find_element_by_id("id_name").send_keys(self.config['test_boot_volume'])
+        Select(driver.find_element_by_id("id_volume_source_type")).select_by_visible_text("Image")
+        Select(driver.find_element_by_id("id_image_source")).select_by_index(1)
         driver.find_element_by_xpath("//input[@type='submit' and @value='Create Volume']").click()
 
         Success_alert = driver.find_element_by_xpath("//p[contains(text(), 'Creating volume')]")
 
-        if "Info:" in Success_alert.text and self.config['test_volume'] in str(Success_alert.text):
+        if "Info:" in Success_alert.text and self.config['test_boot_volume'] in str(Success_alert.text):
              print Success_alert.text
         else:
             raise
-        driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume'] + "')]/td[contains(text(), 'Available')]")
-
-        #Create cinder backup
-        driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume'] + "')]//a[@data-toggle='dropdown']").click()
-        driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume'] + "')]//a[contains(. , 'Create Backup')]").click()
+        #launch a vm instance from volume
+        driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_boot_volume'] + "')]/td[contains(text(), 'Available')]")
+        driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_boot_volume'] + "')]//a[@data-toggle='dropdown']").click()
+        driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_boot_volume'] + "')]//a[contains(. , 'Launch as Instance')]").click()
         driver.find_element_by_id("id_name").clear()
-        driver.find_element_by_id("id_name").send_keys(self.config['test_volume_backup'])
-        driver.find_element_by_xpath("//input[@type='submit' and @value='Create Volume Backup']").click()
-        Success_alert = driver.find_element_by_xpath("//p[contains(text(), 'Creating volume backup ')]")
-        print Success_alert.text
-        if "Success:" in Success_alert.text and self.config['test_volume_backup'] in str(Success_alert.text):
+        driver.find_element_by_id("id_name").send_keys(self.config['test_volume_vm'])
+        driver.find_element_by_xpath("//ul[@role='tablist']//a[text()='Networking']").click()
+        select_network = driver.find_element_by_xpath("//ul[@id='selected_network']").text
+        if select_network == '':
+            driver.find_element_by_xpath("//ul[@id='available_network']/li[1]/a[1]").click()
+
+        driver.find_element_by_xpath("//input[@value='Launch']").click()
+
+        Success_alert = driver.find_element_by_xpath("//p[contains(text(), 'Launched instance')]")
+
+        if "Success" in Success_alert.text and self.config['test_volume_vm'] in str(Success_alert.text):
              print Success_alert.text
         else:
             raise
-        driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume_backup'] + "')]/td[contains(text(), 'Available')]")
-        #Delete Cinder backup
-        driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume_backup'] + "')]//input[@type='checkbox']").click()
-        driver.find_element_by_xpath("//button[contains(., 'Delete Volume Backups')]").click()
-        driver.find_element_by_xpath("//a[contains(text(), 'Delete Volume Backups')]").click()
-        terminate_status = driver.find_element_by_xpath("//p[contains(text(), 'Scheduled deletion of Volume Backup:')]")
+        driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume_vm'] + "')]/td[contains(text(), 'Running')]")
+        table_element = driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume_vm'] + "')]")
+        print table_element.text
+        #delete the testing instance.
+        vm_element = driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume_vm'] + "')]")
+        # print table_element.text
+        vm_element.find_element_by_xpath(".//input[@type='checkbox']").click()
+        driver.find_element_by_xpath("//button[contains(., 'Terminate Instances')]").click()
+        driver.find_element_by_xpath("//a[contains(text(), 'Terminate Instance')]").click()
+        terminate_status = driver.find_element_by_xpath("//p[contains(text(), 'Scheduled termination of Instance:')]")
         print terminate_status.text
-        driver.find_element_by_xpath("//ul[@id='volumes_and_snapshots']//a[contains(., 'Volumes')]").click()
-        #delete Cinder Volume.
-        table_element = driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_volume'] + "')]")
+        #go back to volume tab and delete the test volume.
+        driver.find_element_by_xpath("//dd/div//a[text()='Volumes' and @href='/project/volumes/']").click()
+        table_element = driver.find_element_by_xpath("//tr[contains(@data-display, '" + self.config['test_boot_volume'] + "')]")
         print table_element.text
         table_element.find_element_by_xpath(".//input[@type='checkbox']").click()
         driver.find_element_by_xpath("//button[contains(., 'Delete Volumes')]").click()
